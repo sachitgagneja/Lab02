b09d65d2fed7d6e29906b20461213379cee2b963
Sachit Gagneja <skc7823@autuni.ac.nz>
2020-03-09 17:46:57 +1300

    first commit

---
+++ daysarray.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="Web Development :: Lab 1 /">
+        <meta name="keywords" content="Web,development" />
+        <title>Days Array</title>
+    </head>
+    <body>
+        <?php
+            $days=array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+            echo "The days in English are: ",$days[0],",",$days[1],",",$days[2],",",$days[3],",",$days[4],",",$days[5],",",$days[6],".";
+            $days=["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+            echo "The days in French are: ",$days[0],",",$days[1],",",$days[2],",",$days[3],",",$days[4],",",$days[5],",",$days[6],".";
+        ?>
+    </body>   
+ </html>
---
+++ iseven.php
@@ -0,0 +1,6 @@
+<html>
+    <head>
+        <title>PHP functions</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+    <body>
